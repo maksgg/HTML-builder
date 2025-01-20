ba831e9745b42cb7ba43717cb584237575f6ec2f
Maksym Zhovmir <113468725+maksgg@users.noreply.github.com>
2025-01-20 03:08:02 -0600

    fix 5 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,31 +1,24 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('node:readline');
 
-const stylesFolderPath = path.join(__dirname, 'styles');
-const projectDistFolderPath = path.join(__dirname, 'project-dist');
-const outputFile = path.join(projectDistFolderPath, 'bundle.css');
-
-fs.readdir(stylesFolderPath, (err, files) => {
-  if (err) {
-    console.error('Error reading styles folder:', err);
-    return;
-  }
-
-  const styleFiles = files.filter((file) => {
-    const filePath = path.join(stylesFolderPath, file);
-    const isFile = fs.statSync(filePath).isFile();
-    const hasCorrectExtension = path.extname(file).toLowerCase() === '.css';
-
-    return isFile && hasCorrectExtension;
-  });
-
-  const compiledStyles = styleFiles.map((file) => {
-    const filePath = path.join(stylesFolderPath, file);
-    return fs.readFileSync(filePath, 'utf8');
-  });
-
-  const bundleContent = compiledStyles.join('\n');
-  fs.writeFileSync(outputFile, bundleContent, 'utf8');
-
-  console.log('Styles compiled successfully!');
-});
+const streamToWrite = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'bundle.css'),
+);
+fs.readdir(
+  path.join(__dirname, 'styles'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      if (file.isFile() && path.extname(`${file.name}`) === '.css') {
+        let rl = readline.createInterface({
+          input: fs.createReadStream(path.join(__dirname, 'styles', file.name)),
+        });
+        rl.on('line', (line) => {
+          streamToWrite.write(`${line}\n`);
+        });
+      }
+    });
+  },
+);
